075602e80fb2cd02983f1607bf9971ff89919edd
bwelco <1143955880@qq.com>
2016-08-26 13:40:32 +0800

    clear all

--- src/main/java/com/bwelco/bwelcosearch/JsonStr.java
+++
@@ -1,8 +0,0 @@
-package com.bwelco.bwelcosearch;
-
-/**
- * Created by bwelco on 2016/6/5.
- */
-public class JsonStr {
-    public static String str = "[\"admin\",[\"administrator\",\"adminlte\",\"administration\",\"admin5\",\"administrative\",\"administrative processing\",\"administer\",\"administrator什么意思\",\"administrator权限\",\"admin md5\"]]";
-}
